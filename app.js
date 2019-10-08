67e9dcd9f916c02919e80ef37324d11405e9cd33
Stella <feelis1119@naver.com>
2019-10-07 20:56:06 -0400

    fixingHBS

--- app.js
+++ app.js
@@ -2,19 +2,16 @@ const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
 
+const port = process.env.PORT || 3000;
 const app = express();
-
 app.use(express.static('public'));
 
-const port = process.env.PORT || 3000;
-
 app.set('view engine','hbs');
-app.set('views', path.join(__dirname + "/views"))
-
+app.set('views', path.join( __dirname + "/views"));
 
 
 // app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname + "/views"));
+//     res.render('layout', { test: "Hello!"})
 // })
 
 app.listen(port, () => {
