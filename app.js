0fbb3470c923df1acc9f09cd87414326898184c7
Stella <feelis1119@naver.com>
2019-10-07 20:25:37 -0400

    fixingNPM

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + "/views/layout.hbs"));
-})
+app.set('view engine','hbs');
+app.set('views', path.join(__dirname + "/views"))
+
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname + "/views"));
+// })
 
 app.listen(port, () => {
     console.log(`app is running on ${port}`);
